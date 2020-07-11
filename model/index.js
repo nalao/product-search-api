85889b10f52346755031788dfb21ae3ddae30af2
KILAR <larsonethaly@gmail.com>
2020-07-11 11:10:14 +0700

    amazon cafe

--- model/index.js
+++ model/index.js
@@ -21,28 +21,31 @@ index.getById = (req, res) => {
 index.add = (req, res) => {
   db.query(
     "SELECT COUNT(*) as total FROM product WHERE pro_id = '" +
-      req.body.pro_id +
-      "'",
+    req.body.pro_id +
+    "'",
     function (error, results, fields) {
       if (error) throw error;
       if (results[0].total == 0) {
         db.query(
           "INSERT INTO product (pro_id, name, price, price_send, qty, ccy) VALUES ('" +
-            req.body.pro_id +
-            "','" +
-            req.body.name +
-            "','" +
-            req.body.price +
-            "','" +
-            req.body.price_send +
-            "','" +
-            req.body.qty +
-            "','" +
-            req.body.ccy +
-            "')",
+          req.body.pro_id +
+          "','" +
+          req.body.name +
+          "','" +
+          req.body.price +
+          "','" +
+          req.body.price_send +
+          "','" +
+          req.body.qty +
+          "','" +
+          req.body.ccy +
+          "')",
           function (error, results, fields) {
             if (error) throw error;
-            res(null, { status: true, msg: "ເພີ່ມຂໍ້ມູນສຳເລັດແລ້ວ" });
+            res(null, {
+              status: true,
+              msg: "ເພີ່ມຂໍ້ມູນສຳເລັດແລ້ວ"
+            });
           }
         );
       } else {
@@ -58,20 +61,23 @@ index.add = (req, res) => {
 index.edit = (req, res) => {
   db.query(
     "UPDATE product SET name = '" +
-      req.body.name +
-      "', price = '" +
-      req.body.price +
-      "', price_send = '" +
-      req.body.price_send +
-      "', qty = '" +
-      req.body.qty +
-      "', ccy = '" +
-      req.body.ccy +
-      "' WHERE pro_id = " +
-      req.body.pro_id,
+    req.body.name +
+    "', price = '" +
+    req.body.price +
+    "', price_send = '" +
+    req.body.price_send +
+    "', qty = '" +
+    req.body.qty +
+    "', ccy = '" +
+    req.body.ccy +
+    "' WHERE pro_id = '" +
+    req.body.pro_id + "' ",
     function (error, results, fields) {
       if (error) throw error;
-      res(null, { status: true, msg: "ແກ້ໄຂຂໍ້ມູນສຳເລັດແລ້ວ" });
+      res(null, {
+        status: true,
+        msg: "ແກ້ໄຂຂໍ້ມູນສຳເລັດແລ້ວ"
+      });
     }
   );
 };
@@ -79,8 +85,8 @@ index.edit = (req, res) => {
 index.sale = (req, res) => {
   db.query(
     "SELECT COUNT(*) as total, qty FROM product WHERE pro_id = '" +
-      req.body.pro_id +
-      "'",
+    req.body.pro_id +
+    "'",
     function (error, results, fields) {
       if (error) throw error;
       if (results[0].total > 0) {
@@ -88,12 +94,15 @@ index.sale = (req, res) => {
           const totalQty = results[0].qty - req.body.qty;
           db.query(
             "UPDATE product SET qty = '" +
-              totalQty +
-              "' WHERE pro_id = " +
-              req.body.pro_id,
+            totalQty +
+            "' WHERE pro_id = '" +
+            req.body.pro_id + "' ",
             function (error, results, fields) {
               if (error) throw error;
-              res(null, { status: true, msg: "ການດຳເນີນການສຳເລັດແລ້ວ" });
+              res(null, {
+                status: true,
+                msg: "ການດຳເນີນການສຳເລັດແລ້ວ"
+              });
             }
           );
         } else {
@@ -113,13 +122,16 @@ index.sale = (req, res) => {
 };
 
 index.delete = (req, res) => {
-  db.query("DELETE FROM product WHERE id = " + req.params.id, function (
+  db.query("DELETE FROM product WHERE pro_id = '" + req.params.id + "' ", function (
     error,
     results,
     fields
   ) {
     if (error) throw error;
-    res(null, { status: true, msg: "ລົບຂໍ້ມູນສຳເລັດແລ້ວ" });
+    res(null, {
+      status: true,
+      msg: "ລົບຂໍ້ມູນສຳເລັດແລ້ວ"
+    });
   });
 };
 
